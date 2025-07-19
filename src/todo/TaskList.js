74750e68e37a2bbd0053ae5d072ae3b3e445c321
AjeshMadhavan <101620971+AjeshMadhavan@users.noreply.github.com>
2025-07-19 12:12:27 +0530

    Add: Store todo items in local storage (#1)
    * Refactor: format code
    
    * Refactor: format code and handled title change
    
    * Add: stored todo items in local-storage
    
    ---------
    
    Co-authored-by: Ajesh Madhavan <ajesh.madhavan@vonnue.com>

--- src/todo/TaskList.js
+++ src/todo/TaskList.js
@@ -1,51 +1,61 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-export default function TaskList({todos, onChangeTodo, onDeleteTodo}){
-    return(
-        <>
-        {todos.length > 0 && 
-        <ul className='todo-ul'>
-            {todos.map(todo => (
-                <li key={todo.id} className='todo-li'>
-                    <Task todo = {todo} onChange={onChangeTodo} onDelete={onDeleteTodo}/>
-                </li>
-            ))}
+export default function TaskList({ todos, onChangeTodo, onDeleteTodo }) {
+  return (
+    <>
+      {todos.length > 0 && (
+        <ul className="todo-ul">
+          {todos.map((todo) => (
+            <li key={todo.id} className="todo-li">
+              <Task
+                todo={todo}
+                onChange={onChangeTodo}
+                onDelete={onDeleteTodo}
+              />
+            </li>
+          ))}
         </ul>
-        }
-        </>
-    )
+      )}
+    </>
+  );
 }
 
-function Task({todo, onChange, onDelete}){
-    const [isEditing, setIsEditing] = useState(false);
-    let todoContent;
-    if(isEditing){
-        todoContent = (
-        <>
-            <input 
-            value = {todo.title}
-            className='todo-edit-input'
-            onChange={e => {
-                onChange({
-                    ...todo, 
-                    title: e.target.value
-                })
-            }}
-             />
-            {/* <button onClick={() => setIsEditing(false)} className='todo-save'>save</button> */}
-        </>);
-    } else {
-        todoContent = (
-            <>
-                {todo.title}
-                {/* <button onClick={() => setIsEditing(true)} className='todo-edit'>Edit</button> */}
-            </>
-        );
-    }
-    return(
-       <div className='todo-items-list'> 
-          <label className='todo-items-label'>
-            {/* <input 
+function Task({ todo, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [todoTitle, setTodoTitle] = useState(todo.title);
+
+  let todoContent;
+  if (isEditing) {
+    todoContent = (
+      <>
+        <input
+          value={todoTitle}
+          className="todo-edit-input"
+          onChange={(e) => {
+            setTodoTitle(e.target.value);
+          }}
+        />
+        {/* <button onClick={() => setIsEditing(false)} className='todo-save'>save</button> */}
+      </>
+    );
+  } else {
+    todoContent = (
+      <>
+        {todo.title}
+        {/* <button onClick={() => setIsEditing(true)} className='todo-edit'>Edit</button> */}
+      </>
+    );
+  }
+
+  const onSaveChangeClick = () => {
+    onChange({ ...todo, title: todoTitle });
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="todo-items-list">
+      <label className="todo-items-label">
+        {/* <input 
                 type="checkbox"
                 checked={todo.done}
                 onChange={e => {
@@ -55,18 +65,23 @@ function Task({todo, onChange, onDelete}){
                     })
                 }}
             /> */}
-            <h3 className='todo-item-name'>{todoContent}</h3>
-           </label>
-            <div className='todo-buttons'>
-                {isEditing ? 
-                <button onClick={() => setIsEditing(false)}  className='todo-save'>Save</button>:
-                <button onClick={() => setIsEditing(true)} className='todo-edit'>Edit</button>}
+        <h3 className="todo-item-name">{todoContent}</h3>
+      </label>
+      <div className="todo-buttons">
+        {isEditing ? (
+          <button onClick={onSaveChangeClick} className="todo-save">
+            Save
+          </button>
+        ) : (
+          <button onClick={() => setIsEditing(true)} className="todo-edit">
+            Edit
+          </button>
+        )}
 
-                <button onClick={()=> onDelete(todo.id)} className='todo-delete'>
-                    Delete
-                </button>
-
-            </div>
-       </div>
-    )
+        <button onClick={() => onDelete(todo.id)} className="todo-delete">
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
